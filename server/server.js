e9a2a458dcc0f8ee16171a8036408004f649bdb1
x7k3e21 <uly4nkin.denis@gmail.com>
2024-06-12 00:17:16 +0300

    Finished handlers, fixed typos, cleaned code

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const express = require("express");
 const application = express();
 
 function handlerTimestamp(request, response, handler) {
-
+    console.log(`Request timestamp: ${new Date(Date.now()).toUTCString()}`);
 
     handler();
 }
 
 function handlerRoute(request, response, handler) {
-
+    console.log(`Request route: ${request.baseUrl}`);
 
     handler();
 }
@@ -18,17 +18,19 @@ function handlerRoute(request, response, handler) {
 application.use(handlerTimestamp);
 application.use(handlerRoute);
 
-const mongodb = requrie("mongodb");
+application.set("view engine", "hbs");
+
+const mongodb = require("mongodb");
 
 const MONGODB_ADDRESS = process.env.MONGODB_ADDRESS || "127.0.0.1";
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
 
 const mongoConnection = `mongodb://${MONGODB_ADDRESS}:${MONGODB_PORT}`;
 
-const mongoClient = mongodb.MongoClient(mongoConnection);
+const mongoClient = new mongodb.MongoClient(mongoConnection);
 
 application.get("/", (request, response) => {
-
+    response.send("Hello, world!");
 });
 
 const SERVER_PORT = process.env.SERVER_PORT || 3030;
